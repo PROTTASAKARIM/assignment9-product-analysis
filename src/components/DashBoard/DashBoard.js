148d0d4a528f5b7317c36af4105c185e4d651150
PROTTASAKARIM <prottasakarim99@gmail.com>
2022-04-05 05:22:42 +0600

    chart added

--- src/components/DashBoard/DashBoard.js
+++ src/components/DashBoard/DashBoard.js
@@ -1,9 +1,111 @@
 import React from 'react';
+import { Area, AreaChart, Bar, BarChart, CartesianGrid, Legend, Line, LineChart, Pie, PieChart, Tooltip, XAxis, YAxis } from 'recharts';
+import './DashBoard.css'
+
+const data =
+    [
+        {
+            "month": "Mar",
+            "investment": 100000,
+            "sell": 241,
+            "revenue": 10401
+        },
+        {
+            "month": "Apr",
+            "investment": 200000,
+            "sell": 423,
+            "revenue": 24500
+        },
+        {
+            "month": "May",
+            "investment": 500000,
+            "sell": 726,
+            "revenue": 67010
+        },
+        {
+            "month": "Jun",
+            "investment": 500000,
+            "sell": 529,
+            "revenue": 40405
+        },
+        {
+            "month": "Jul",
+            "investment": 600000,
+            "sell": 601,
+            "revenue": 50900
+        },
+        {
+            "month": "Aug",
+            "investment": 700000,
+            "sell": 670,
+            "revenue": 61000
+        }
+    ]
+
 
 const DashBoard = () => {
     return (
         <div>
-            <h1>this is my DashBoard</h1>
+            <div className='chart-style'>
+                <div className='linechart-style'>
+                    <LineChart width={400} height={400} data={data}>
+                        <Line dataKey={'sell'}></Line>
+                        <CartesianGrid stroke="#ccc" />
+                        <Tooltip />
+                        <XAxis dataKey={'month'}></XAxis>
+                        <YAxis></YAxis>
+                    </LineChart>
+                </div>
+                <div>
+                    <BarChart
+                        width={400}
+                        height={400}
+                        data={data}
+                        margin={{
+                            top: 5,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="investment" fill="#8884d8" />
+                        <Bar dataKey="revenue" fill="#82ca9d" />
+                    </BarChart>
+                </div>
+            </div>
+            <div className='chart-style'>
+                <div className='linechart-style'>
+                    <AreaChart
+                        width={400}
+                        height={400}
+                        data={data}
+                        margin={{
+                            top: 10,
+                            right: 30,
+                            left: 0,
+                            bottom: 0,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="month" />
+                        <YAxis />
+                        <Tooltip />
+                        <Area type="monotone" dataKey="investment" stroke="#8884d8" fill="#8884d8" />
+                    </AreaChart>
+                </div>
+                <div>
+                    <PieChart width={400} height={400}>
+                        <Pie data={data} dataKey="investment" cx="50%" cy="50%" outerRadius={60} fill="#8884d8" />
+                        <Pie data={data} dataKey="revenue" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label />
+                    </PieChart>
+                </div>
+            </div>
+
         </div>
     );
 };
